d173f40f0d351b5104713e7235d8379e6df1eeec
brazilec22 <stratienko.artem@gmail.com>
2020-11-26 22:25:01 +0300

    auto commit rep - hackHere - DevPost

--- app/client/agentCoronas/agentCoronas/Application/Controller/SettingUser.swift
+++ app/client/agentCoronas/agentCoronas/Application/Controller/SettingUser.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
-
+import SceneKit
 class SettingUser: UIViewController {
-
+    // SceneKit scene
+    @IBOutlet weak var scene: SCNView!
+    var sceneNode: SCNScene!
+          var cameraNode: SCNNode!
+          var camera: SCNCamera!
+          var playerNode: SCNNode!
+          var officeNode: SCNNode!
+          var tokens = [SCNNode]()
+          var ambientLightNode: SCNNode!
+          var ambientLight: SCNLight!
+          var omniLightNode: SCNNode!
+          var omniLight: SCNLight!
+          var sceneRect: CGRect!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupSceneView()
+        self.initializeHideKeyboard()
         // Do any additional setup after loading the view.
     }
-
-
 }
+
+
+ extension SettingUser {
+    func initializeHideKeyboard()  {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self,
+                                                             action: #selector(dismissMyKeyboard))
+        //Add this tap gesture recognizer to the parent view
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissMyKeyboard() {
+        view.endEditing(true)
+    }
+ }
 
